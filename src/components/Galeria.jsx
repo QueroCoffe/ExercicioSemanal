5e7c7e1dae1b2f2ddee6ae45b758dac36ca2df57
William dos Santos Rodrigues <williambfs2011@gmail.com>
2024-07-05 20:57:10 -0300

    atualização da galeria

--- src/components/Galeria.jsx
+++ src/components/Galeria.jsx
@@ -1,10 +1,15 @@
 import "./Galeria.css";
 
 const Jogos = [
-    { cod: 1, nomeDojogo: "EA FC 2024", ano: 2023, imagem:"/img/EAFC24.jpg"},
-    { cod: 2, nomeDojogo: "Call of Duty", ano: 2024, imagem:"/img/Callofduty.jpg" },
-    { cod: 3, nomeDojogo: "GTA VI", ano: 2025, imagem:"/img/gtav.jpg" },
-    { cod: 4, nomeDojogo: "Street Fighter", ano: 2024 ,imagem:"/img/street.jpg"}
+    { cod: 1, nomeDojogo: "EA FC 2024", ano: 2023, imagem: "/img/EAFC24.jpg" },
+    { cod: 2, nomeDojogo: "Call of Duty", ano: 2024, imagem: "/img/Callofduty.jpg" },
+    { cod: 3, nomeDojogo: "GTA VI", ano: 2025, imagem: "/img/gtav.jpg" },
+    { cod: 4, nomeDojogo: "Street Fighter", ano: 2024, imagem: "/img/street.jpg" },
+    { cod: 5, nomeDojogo: "Mario Kart", ano: 2018, imagem: "/img/mariokart.jpg" },
+    { cod: 6, nomeDojogo: "Elder Ring", ano: 2023, imagem: "/img/elderring.jpg" },
+    { cod: 7, nomeDojogo: "Hollow Knight", ano: 2016, imagem: "/img/hollowknigth.jpg" },
+    { cod: 8, nomeDojogo: "God of War", ano: 2023, imagem: "/img/godofwar.jpg" },
+    { cod: 9, nomeDojogo: "Legue of Legends", ano:2014, imagem:"/img/lol.jpg"}
 ];
 
 function Galeria() {
@@ -12,7 +17,7 @@ function Galeria() {
     const cardsJogos = Jogos.map((jogo) => {
         return (
             <article key={jogo.cod} className="game-card">
-                <img src={jogo.imagem} alt={jogo.nomeDojogo} width={300}/>
+                <img src={jogo.imagem} alt={jogo.nomeDojogo} width={300} />
                 <h3 className="game-title">{jogo.nomeDojogo}</h3>
                 <p className="game-year">Ano: {jogo.ano}</p>
                 <button className="play-button" aria-label={`Jogue ${jogo.nomeDojogo} agora`}>Jogue Agora</button>
